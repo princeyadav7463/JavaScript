ad972bbe6e5b565bedf9a0d02047aadaa598fe8e
princeyadav7463 <143525918+princeyadav7463@users.noreply.github.com>
2025-03-02 21:58:04 +0530

    Add files via upload

---
+++ part1/changes.js
@@ -0,0 +1,11 @@
+let gameName = "Spiderman";
+
+gameName = "batman";
+console.log(gameName);
+
+let username = "Princedotcom"
+ 
+username = "Prince";
+
+console.log(username);
+
---
+++ part1/opePre.js
@@ -0,0 +1,6 @@
+let score = 2 * 3 + 2- 1;
+
+let score1 = (2 * (3 + 2 )- 1);
+
+console.log(score);
+console.log(score1);
